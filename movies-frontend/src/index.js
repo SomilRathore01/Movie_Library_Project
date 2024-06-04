5bea2a65a9ca331265c63cfa22b2427daa3ffa4f
SomilRathore01 <112357002+SomilRathore01@users.noreply.github.com>
2024-06-04 12:01:17 +0530

    Update index.js

--- movies-frontend/src/index.js
+++ movies-frontend/src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux'
 import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-axios.defaults.baseURL = "http://localhost:3000"
+axios.defaults.baseURL = `${window.location.origin}`
 root.render(
   <React.StrictMode>
     <BrowserRouter>
